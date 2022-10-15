919c32a8702f970cb0eb4407eb26165a866176ce
josue nelson <josuenelson0@gmail.com>
2022-10-15 13:08:36 +0200

    ajout des tables categories et logement

---
+++ TABLES/IP_categorie.sql
@@ -0,0 +1,6 @@
+DROP TABLE CATEGORIE
+create table CATEGORIE (
+id_categorie int PRIMARY KEY,
+type varchar(255),
+charge_forfaitaire int 
+);
---
+++ TABLES/IP_logement.sql
@@ -0,0 +1,4 @@
+drop table LOGEMENT;
+create table LOGEMENT (
+id_logement int PRIMARY KEY
+);
