9a5e4fd5df97492418f8f4779a85568409156cfb
dev-scott <95773992+dev-scott@users.noreply.github.com>
2022-10-18 20:46:42 +0200

    ajout des tables adresse et achat

--- TABLES/PROPRIETAIRE.sql
+++
@@ -1,8 +0,0 @@
-drop table PROPRIETAIRE;
-
-create table PROPRIETAIRE (
-id_proprietaire int PRIMARY KEY,
-nom_proprietaire varchar(255) not null,
-prenom_proprietaire varchar(255) not null,
-num_tel_proprietaire int
-);
