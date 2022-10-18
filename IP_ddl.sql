9a257da684234adca21525af1a3bee5111a6b910
josue nelson <josuenelson0@gmail.com>
2022-10-18 15:06:22 +0200

    Ajout de IP_ddl.sql

---
+++ IP_ddl.sql
@@ -0,0 +1,23 @@
+PROMPT Creating tables, constraints and views for Customer Orders
+
+
+/*execution des tables*/
+
+@@TABLES/IP_tables.sql
+-- @@TABLES/IP_adresse.sql
+-- @@TABLES/IP_categorie.sql
+-- @@TABLES/IP_client.sql
+-- @@TABLES/IP_historique.sql
+-- @@TABLES/IP_logement.sql
+-- @@TABLES/IP_proprietaire.sql
+-- @@TABLES/IP_publication.sql
+-- @@TABLES/IP_reservation.sql
+
+
+
+
+/*@@co_views.sql*/
+
+/*@@co_constraints.sql*/
+
+/*@@co_comments.sql*/
